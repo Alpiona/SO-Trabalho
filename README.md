a595458eecdfb5c35110a8017a69fa17bdd82e61
Alpiona <alpiona@hotmail.com>
2017-07-23 17:39:08 -0300

    Parte 3(spsh.c) falta apenas redirecionamento

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # SO-Trabalho
 
 Parte 3:
-  Consegui botar para ler o executavel seguido dos seus parametros ("ls -lht", "ps -aux"), falta botar a parte de pipe e background.
-
-  Refiz o spsh.c, está lendo em background e varios argumentos em um mesmo comando,
-  tentando adicionar pipe, e depois ver redirecionamento de entrada e saida
+  Funcionando background e pipe, falta apenas redirecionamento de entrada saida para arquivos
--- spsh.c
+++ spsh.c
@@ -9,37 +9,75 @@
 
 int pid;
 
-void executaComando(char* linha){
-  int qntComando=1,j=0, k=0;;
+void executaPipe(int in, int out, char* cmd[]){
+  int pid;
+  if ((pid = fork())== 0){
+    if (in != 0) {
+        dup2 (in, 0);
+        close (in);
+      }
+
+    if (out != 1) {
+        dup2 (out, 1);
+        close (out);
+    }
+    execvp (cmd[0],cmd);
+  }
+}
+
+void executaComando (int n, int p, char* cmd[n][p]){
+  int in = 0, fd[2];
+  int i;
+  for(i =0;i<n-1;i++){
+    pipe(fd);
+    executaPipe(in,fd[1],cmd[i]);
+    close(fd[1]);
+    in = fd[0];
+  }
+  if (in != 0)
+    dup2(in,0);
+  execvp(cmd[i][0],cmd[i]);
+}
+
+void preparaComando(char* linha){
+  int qntComando=1,j=0, k=0, y=0;
   char executavel[COMANDOTAM];
   for(int i=0; i<strlen(linha);i++){
     if (linha[i] == '|'){
       qntComando++;
     }
   }
-  char comando[ARGSTAM][COMANDOTAM];
+  char comando[ARGSTAM*qntComando][COMANDOTAM];
   char* aux[ARGSTAM];
-  char** cmd[qntComando];
+  char* cmd[qntComando][ARGSTAM];
   for (int i=0; i<strlen(linha);i++){
     executavel[i-k] = linha[i];
     if (linha[i] == ' ' || i+1 == strlen(linha)){
       executavel[i-k] = 0;
-      k=i+1;
-      strcpy (comando[j],executavel);
-      aux[j] = (char*)comando[j];
+      strcpy (comando[j+(y*qntComando)],executavel);
+      aux[j] = (char*)comando[j+(y*qntComando)];
+      cmd[y][j] = aux[j];
       j++;
+      if(linha[i+1]== '|'){
+        aux[j]=NULL;
+        cmd[y][j] = NULL;
+        i+=2;
+        j=0;
+        y++;
+      }
+      k=i+1;
     }
   }
   aux[j]=NULL;
-  execvp(aux[0], aux);
-  // char** cmd[qntComando];
+  cmd[y][j]=NULL;
+  executaComando(qntComando, ARGSTAM, cmd);
 }
 
 void executaLinha(char* linha){
-  linha[strlen(linha)] = 0; //Tira o newline no final
+  linha[strlen(linha)] = 0;
   int k=0;;
   char comando[COMANDOTAM], ch;
-  for (int i=0;i<strlen(linha);i++){  //Executa cada comando até achar EOF ou &
+  for (int i=0;i<strlen(linha);i++){
     comando[i-k] = linha[i];
     if (linha[i] == '&' || i+1 == strlen(linha)){
       if(linha[i] == '&')
@@ -49,11 +87,11 @@ void executaLinha(char* linha){
       if (linha[i] == '&'){
         pid = fork();
         if (pid == 0){
-          executaComando(comando);
+          preparaComando(comando);
         }
       }
       else{
-        executaComando(comando);
+        preparaComando(comando);
       }
       for (k=0; k<COMANDOTAM;k++){
         comando[k]=0;
@@ -66,7 +104,8 @@ void executaLinha(char* linha){
 
 int main(){
   char linha[LINHATAM];
-  fprintf(stderr, "spsh.%s ", getenv("PWD"));
-  fgets(linha, sizeof linha, stdin);
+  // fprintf(stderr, "spsh.%s ", getenv("PWD"));
+  // fgets(linha, sizeof linha, stdin);
+  strcpy(linha,"ps -aux | grep kworker\n");
   executaLinha(linha);
 }

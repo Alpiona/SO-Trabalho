355ab700ad74f9e55accf8e4a8332299b0112897
Alpiona <alpiona@hotmail.com>
2017-07-23 15:12:45 -0300

    Parte 3 (spsh.c) - refeita mas ainda incompleta

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Parte 3:
   Consegui botar para ler o executavel seguido dos seus parametros ("ls -lht", "ps -aux"), falta botar a parte de pipe e background.
+
+  Refiz o spsh.c, está lendo em background e varios argumentos em um mesmo comando,
+  tentando adicionar pipe, e depois ver redirecionamento de entrada e saida
--- spsh.c
+++ spsh.c
@@ -3,32 +3,70 @@
 #include <string.h>
 #include <unistd.h>
 
-#define LINETAM 100
-#define ARGSTAM 10
+#define LINHATAM 100
+#define COMANDOTAM 20
+#define ARGSTAM 5
+
+int pid;
+
+void executaComando(char* linha){
+  int qntComando=1,j=0, k=0;;
+  char executavel[COMANDOTAM];
+  for(int i=0; i<strlen(linha);i++){
+    if (linha[i] == '|'){
+      qntComando++;
+    }
+  }
+  char comando[ARGSTAM][COMANDOTAM];
+  char* aux[ARGSTAM];
+  char** cmd[qntComando];
+  for (int i=0; i<strlen(linha);i++){
+    executavel[i-k] = linha[i];
+    if (linha[i] == ' ' || i+1 == strlen(linha)){
+      executavel[i-k] = 0;
+      k=i+1;
+      strcpy (comando[j],executavel);
+      aux[j] = (char*)comando[j];
+      j++;
+    }
+  }
+  aux[j]=NULL;
+  execvp(aux[0], aux);
+  // char** cmd[qntComando];
+}
+
+void executaLinha(char* linha){
+  linha[strlen(linha)] = 0; //Tira o newline no final
+  int k=0;;
+  char comando[COMANDOTAM], ch;
+  for (int i=0;i<strlen(linha);i++){  //Executa cada comando até achar EOF ou &
+    comando[i-k] = linha[i];
+    if (linha[i] == '&' || i+1 == strlen(linha)){
+      if(linha[i] == '&')
+        comando[i-k] = 0;
+      else
+        comando[i-k+1] = 0;
+      if (linha[i] == '&'){
+        pid = fork();
+        if (pid == 0){
+          executaComando(comando);
+        }
+      }
+      else{
+        executaComando(comando);
+      }
+      for (k=0; k<COMANDOTAM;k++){
+        comando[k]=0;
+      }
+      i++;
+      k = i+1;
+    }
+  }
+}
 
 int main(){
-  char line[LINETAM];
-  char aux[15];
-  char args[ARGSTAM][15];
-  char* args2[ARGSTAM];
-  int j = 0, k=0;
+  char linha[LINHATAM];
   fprintf(stderr, "spsh.%s ", getenv("PWD"));
-  fgets(line, sizeof line, stdin);
-  line[strlen(line)-1] = 0;
-  for (int i=0; i<strlen(line);i++){
-    if (line[i] != ' '){
-      aux[i-k] = line[i];
-    }
-    else{
-      strcpy(args[j],aux);
-      j++;
-      k=i+1;
-    }
-  }
-  strcpy(args[j],aux);
-  for (int i=0;i<ARGSTAM;i++){
-    args2[i] = args[i];
-  }
-  args2[j+1] = NULL;
-  execvp(args2[0],args2);
+  fgets(linha, sizeof linha, stdin);
+  executaLinha(linha);
 }

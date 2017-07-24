88057c3df23d92e0912ff1993724aaa67634ef1d
Alpiona <alpiona@hotmail.com>
2017-07-23 21:45:18 -0300

    Parte4 completo

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # SO-Trabalho
 
 Parte 2:
-  Terminada
+  Completo
 
 Parte 3:
   Praticamente completo, por algum motivo, pipe com redirecionamento ta dando "stack smashing detected", mas foi o unico erro que achei. Vou tentar fazer a parte 4 e depois volto pra ele
 
 Parte 4:
-  As sequências de acontecimento estão funcionando, falta adicionar alguem ser preso aleatoriamente e o nomed pipe
+  Completo
--- parte4.c
+++ parte4.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
 
-int fila[15], nEspaco, nMatagal;
+int fila[15], nEspaco, nMatagal, foiPego, alvo;
 sem_t espacoBarco, matagal, barcoSaiu, barcoAtravessou;
 
+void criarNamedPipe (){
+  int fd;
+  char* filename = "Parte5-FIFO";
+  char* msg = "“O caminho mais curto nem sempre é o melhor ...”\n";
+  mkfifo(filename, 0666);
+  fd = open(filename, O_WRONLY);
+  write(fd, msg, strlen(msg));
+  close(fd);
+  unlink(filename);
+  return;
+}
 
 void jovem (int arg){
   printf("Jovem %d vai tentar pegar o barco...\n", arg+1);
@@ -19,8 +33,11 @@ void jovem (int arg){
     }
     fila[arg] = aux+1;
     printf("Jovem %d entrou na fila, tem %d na frente dele\n",arg+1, fila[arg]);
-    while (fila[arg] > 3){
-
+    while (fila[arg] > 0){
+      if (foiPego == 1){
+        printf("Jovem %d: Opa! Os seguranças pegaram um ... não vou poder entrar no show... o jeito vai ser o telão mesmo!\n", arg+1);
+        return;
+      }
     }
   }
   sem_wait(&espacoBarco);
@@ -29,7 +46,17 @@ void jovem (int arg){
   printf("Jovem %d sai com o barco.\n", arg+1);
   sem_wait(&barcoAtravessou);
   sem_wait(&matagal);
+  if(foiPego == 1){
+    printf("Jovem %d: Opa! Os seguranças pegaram um ... não vou poder entrar no show... o jeito vai ser o telão mesmo!\n", arg+1);
+    return;
+  }
   printf("Jovem %d atravessando matagal...\n", arg+1);
+  if (arg+1 == alvo){
+    foiPego=1;
+    printf("Jovem %d preso na cerca... Shii! Os seguranças me pegaram!\n",arg+1);
+    sem_post(&matagal);
+    return;
+  }
   sleep(1);
   printf("Jovem %d entrou no show...\n",arg+1);
   nMatagal--;
@@ -52,7 +79,10 @@ void barco (void* arg){
     }
     printf("Barqueiro espera todos atravessarem o matagal...\n");
     while(nMatagal != 0){
-
+      if (foiPego == 1){
+        printf("Opa! Problemas! Os seguranças pegaram um! Acabou meu esquema...\n");
+        criarNamedPipe();
+      }
     }
     printf("Tudo certo! Barqueiro retorna para fazer outra viagem...\n");
     nMatagal = 3;
@@ -71,6 +101,9 @@ int main (){
   for (int i =0; i<15;i++){
     fila[i] = 0;
   }
+  srand(time(NULL));
+  alvo = rand() % 15;
+  foiPego = 0;
   nEspaco = 3;
   nMatagal = 3;
   pthread_t jovens[15], barqueiro;
